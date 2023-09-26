47f56b24c784d98cd83d305f6e0a5af2e8dd6e4f
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-09-26 18:02:39 +0500

    Add project files.

---
+++ 12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = i++;
+            int k = ++j;
+            Console.Write("i=" + i + "\\");
+            Console.Write("j=" + j);
+            Console.Write("k=" + k);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tyuiu.ChalkovaE.M.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChalkovaE.M.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 1 число = ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 2 число = ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 3 число = ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Значение выражения ({n1} + {n2} + {n3}) * 5 = " + DataService.Calc(n1, n2, n3));
+            Console.ReadKey();
+        }
+    }
+}
